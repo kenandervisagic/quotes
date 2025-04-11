3cb4559cd8bfafffd0ca9044f9691728ecef0181
Kenan Dervisagic <notkenan@gmail.com>
2025-04-11 13:40:11 +0200

    refactor

--- docker-tls-setup.sh
+++ docker-tls-setup.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-# Export environment variables from GitHub Secrets
+# Export Docker TLS settings
 export DOCKER_TLS_VERIFY=1
-export DOCKER_HOST=${{ secrets.DOCKER_HOST }}
-export DOCKER_CERT_PATH=${GITHUB_WORKSPACE}/.docker  # Use GitHub Actions workspace
+export DOCKER_CERT_PATH=${GITHUB_WORKSPACE}/.docker
+export DOCKER_HOST=$DOCKER_HOST
 
 # Create the directory to store Docker certs
 mkdir -p $DOCKER_CERT_PATH
 
-# Decode the base64 encoded certificates and save them to the appropriate files
-echo ${{ secrets.CERT_PEM }} | base64 --decode > $DOCKER_CERT_PATH/cert.pem
-echo ${{ secrets.KEY_PEM }} | base64 --decode > $DOCKER_CERT_PATH/key.pem
-echo ${{ secrets.CA_PEM }} | base64 --decode > $DOCKER_CERT_PATH/ca.pem
+# Decode the base64 encoded certificates and save them
+echo "$CERT_PEM" | base64 --decode > $DOCKER_CERT_PATH/cert.pem
+echo "$KEY_PEM" | base64 --decode > $DOCKER_CERT_PATH/key.pem
+echo "$CA_PEM" | base64 --decode > $DOCKER_CERT_PATH/ca.pem
 
-# Print message to indicate successful setup
 echo "Docker TLS setup complete"
