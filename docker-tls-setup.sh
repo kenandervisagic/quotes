38f9d859750b10e909f7e6f5558e1b68d1ada00b
Kenan Dervisagic <notkenan@gmail.com>
2025-04-11 13:41:33 +0200

    refactor

--- docker-tls-setup.sh
+++ docker-tls-setup.sh
@@ -12,5 +12,5 @@ mkdir -p $DOCKER_CERT_PATH
 echo "$CERT_PEM" | base64 --decode > $DOCKER_CERT_PATH/cert.pem
 echo "$KEY_PEM" | base64 --decode > $DOCKER_CERT_PATH/key.pem
 echo "$CA_PEM" | base64 --decode > $DOCKER_CERT_PATH/ca.pem
-
+printenv
 echo "Docker TLS setup complete"
