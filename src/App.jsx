11d879a33a0c5c03fd71ce913e316140374341db
Kenan Dervisagic <notkenan@gmail.com>
2025-04-13 17:49:44 +0200

    Add missing IG link

--- src/App.jsx
+++ src/App.jsx
@@ -195,7 +195,7 @@ function App() {
                     {/* Add TikTok link below */}
                     <div className="footer-message">
                         <p>Want to explore other anonymous messages? <br/>
-                            Check them out on our <a href="https://www.tiktok.com/@the_words_left_behind" target="_blank" rel="noopener noreferrer">TikTok</a> or  <a href="https://www.tiktok.com/@the_words_left_behind" target="_blank" rel="noopener noreferrer">Instagram</a>.
+                            Check them out on our <a href="https://www.tiktok.com/@the_words_left_behind" target="_blank" rel="noopener noreferrer">TikTok</a> or  <a href="https://www.instagram.com/the_words_left_behind" target="_blank" rel="noopener noreferrer">Instagram</a>.
                         </p>
                     </div>
                 </div>
