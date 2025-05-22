1d0acbd5dbf68a776d5c195366d515a12abcebe3
muamer_fatic <muamer.fatic@serapion.net>
2025-05-22 10:27:20 +0200

    Frontend: Removed: MUI's Alert component from SnackBar to provide consisent UI/UX. Added: functionality for setting background-color of SnackBar based on provided severity prop.

--- frontend/src/components/SnackBar/SnackBar.jsx
+++ frontend/src/components/SnackBar/SnackBar.jsx
@@ -1,24 +1,47 @@
-import React, {useState} from 'react';
-import {Alert, Snackbar} from "@mui/material";
+import React, {useEffect, useState} from 'react';
+import {Box, Snackbar} from "@mui/material";
 
-function SnackBar({severity="info", message="User informed."}) {
+function SnackBar({severity = "info", message = "User informed."}) {
     const [isOpen, setIsOpen] = useState(true);
-    const handleClose=()=>{
+    const handleClose = () => {
         setIsOpen(false)
     }
+    const [backgroundColor, setBackgroundColor] = useState("var(--color-info)");
+
+    useEffect(() => {
+        switch (severity) {
+            case "success":
+                setBackgroundColor("var(--color-success)");
+                break;
+            case "error":
+                setBackgroundColor("var(--color-error)");
+                break;
+            case "warning":
+                setBackgroundColor("var(--color-warning)");
+                break;
+            default:
+                setBackgroundColor("var(--color-info)");
+                break;
+        }
+    }, [severity]);
+
+
     return (
         <Snackbar
             autoHideDuration={3000}
             open={isOpen}
             onClose={handleClose}
         >
-            <Alert
-                severity={severity}
-                variant="filled"
-                sx={{width: '100%'}}
-            >
+            <Box sx={{
+                width: '100%',
+                backgroundColor: backgroundColor,
+                padding: '6px 16px',
+                color: 'white',
+                fontWeight: '500',
+                borderRadius: '4px'
+            }}>
                 {message}
-            </Alert>
+            </Box>
         </Snackbar>
     );
 }
