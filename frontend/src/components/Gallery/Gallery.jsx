f1b07fe2eee55824534856731a5dadfd86d06549
Kenan Dervisagic <notkenan@gmail.com>
2025-09-03 10:11:30 +0200

    build images in parallel

--- frontend/src/components/Gallery/Gallery.jsx
+++ frontend/src/components/Gallery/Gallery.jsx
@@ -4,6 +4,7 @@ import "./Gallery.css";
 
 function Gallery() {
     const [images, setImages] = useState([]);
+
     const [nextStartAfter, setNextStartAfter] = useState(null); // { id, likes } or null
     const [isLoading, setIsLoading] = useState(false);
     const [hasMore, setHasMore] = useState(true);
